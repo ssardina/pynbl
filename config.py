476bb8a74ab84063e9f5c276cd08fa2f34f669af
ssardina <ssardina@gmail.com>
2022-06-29 00:00:14 +1000

    pivot table almost there

--- config.py
+++ config.py
@@ -2,3 +2,8 @@
 URL_LIVESTATS = 'https://livestats.dcd.shared.geniussports.com/data'
 
 URL_FIBA_LIVESTATS = 'http://www.fibalivestats.com/data'
+
+
+
+ACT_NON_STATS = ['period', 'game', 'substitution']
+ACTSSUB_NON_STATS = ['startperiod']
