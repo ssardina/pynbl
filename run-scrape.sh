009f4edf4200ab34e5ad1a1b9270e2f5b1c09eb4
ssardina <ssardina@gmail.com>
2022-12-16 04:32:28 +0000

    Script for running the update with gdrive

--- run-scrape.sh
+++ run-scrape.sh
@@ -11,11 +11,11 @@ DIR_SCRIPT=`dirname $0` # Find path of the current script
 ##### SCRIPT STARTS HERE
 now=`date +"%Y-%m-%d--%H:%M"`
 log_file="backup-ssardina-${now}.log"
-EXCLUDE_FILE="~/rsync-exclude-home.txt"
-
 
 cd "$DIR_SCRIPT"
-python -m nbl.nbl_scrapper --games games_22_23 --data test --save
+google-drive-ocamlfuse gdrive
+python -m nbl.nbl_scrapper --games games_22_23 --data gdrive
+fusermount -u gdrive
 
 
 
